0c613341998b460b22ce5753517725de84818c88
sariavaal <sariavaal@gmail.com>
2024-02-14 20:19:32 -0300

    desestructuracion

---
+++ Desestructuración/script.js
@@ -0,0 +1,68 @@
+//Para los siguientes cinco bloques de código, predice la salida. Si una línea de código pasa por un error, indica el error.
+// problema 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+//salida
+//Tesla, Mercedes
+// problema 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+//salida
+//Error name is not defined
+
+//problema 3.
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+//salida
+//12345
+//undefined
+
+//problema 4.
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+//salida
+//false
+//true
+
+//problema 5.
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+//salida
+//value
+//[ 1, 5, 1, 8, 3, 3 ]
+//1
+//5
